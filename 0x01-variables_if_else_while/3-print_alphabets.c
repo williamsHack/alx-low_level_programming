3e079cae2afd4a451160a70ad638d351f0dd303e
williamsHack <williamyawdickson1998@gmail.com>
2022-12-09 13:59:26 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
 for (letter = 'A'; letter <= 'Z'; letter++)
-putchar(latter);
+putchar(letter);
 
 putchar('\n');
 return (0);
