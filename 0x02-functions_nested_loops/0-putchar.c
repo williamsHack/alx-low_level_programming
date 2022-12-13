159f5e3b8aa470035fe3169b2edda2cdfdd78d4c
williamsHack <williamyawdickson1998@gmail.com>
2022-12-13 05:15:43 -0800

    putchat.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "_putchar.h"
 
 /**
  *main - prints the  message
@@ -6,6 +6,15 @@
  */
 int main(void)
 {
-print_message();
+_putchar('H');
+_putchar('y');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t')
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
