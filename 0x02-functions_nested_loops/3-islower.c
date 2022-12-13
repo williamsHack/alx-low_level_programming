d6a7ee1171f12b8631707690791c0fe08c6f1dd5
williamsHack <williamyawdickson1998@gmail.com>
2022-12-13 09:26:26 -0800

    hello

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,4 @@
-#include "main.h"
-#include <ctype.h>
+#include "main.h."
 
 /**
  *_islower - tells if a data is lower or not
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -3,7 +3,6 @@
 
 /*
  * File: holberton.h
- * Auth: Brennan D Baraban
  * Desc: Header file containing declarations for all functions
  *       used in the 0x02-functions_nested_loops directory.
  */
