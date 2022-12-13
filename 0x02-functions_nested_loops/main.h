14f7e36506b2392a3073f2cdabe1c8a177a18367
williamsHack <williamyawdickson1998@gmail.com>
2022-12-13 08:18:01 -0800

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,5 @@
-#include "main.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+int main(void);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
