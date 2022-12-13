955bb4bf647bd7f669987de3df4764d302b53b98
williamsHack <williamyawdickson1998@gmail.com>
2022-12-13 03:46:47 -0800

    Number 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - prints the  message
+ * Return: return the status code
+ */
+int main(void)
+{
+print_message();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code
+ * print_alphabet - print the alphabet in the screen
+ * Return: return status code
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
